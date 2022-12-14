601e78c18e5ce69e67dfcb02f21ed46fb359b4bd
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-12-14 12:38:46 +0600

    added vedio section

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import OurTeam from './OurTeam';
 
 const Home = () => {
     return (
         <div className='mb-8'>
             <h2 className='uppercase'>This is Home</h2>
+            <Banner></Banner>
             <OurTeam></OurTeam>
         </div>
     );
