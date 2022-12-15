3c2c2782cecaeca3d505e021f10d0dc016c6b815
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-12-15 21:36:35 +0600

    startted team section

--- src/Pages/Home/Home/OurTeam.js
+++ src/Pages/Home/Home/OurTeam.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import img1 from '../../../assets/ourteam/masud.jpeg';
+import img2 from '../../../assets/ourteam/mohin.jpeg';
+import img3 from '../../../assets/ourteam/shamim.png';
+import img4 from '../../../assets/ourteam/forida.jpg';
+import img5 from '../../../assets/ourteam//quote-img.png';
+
+import '../Home/OurTeam.css';
 
 const OurTeam = () => {
   return (
