0b8881eb99a3fbe6dd93a7a95a62a657ed40f16b
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-12-21 13:25:51 +0600

    started creadit transfer section

---
+++ src/Pages/Home/Home/CreditTransfer/CreditTransfer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const creditTransfer = () => {
+    return (
+        <div>
+            <h1 className='text-white text-5xl text-center'>This is Credit transfer section</h1>
+        </div>
+    );
+};
+
+export default creditTransfer;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../Banner/Banner';
 import EffectiveVisaSolution from '../EffectiveVisaSolution/EffectiveVisaSolution';
 
+import CreditTransfer from './CreditTransfer/CreditTransfer';
 import FeaturedServices from './FeaturedServices/FeaturedServices';
 import OurTeam from './OurTeam';
 import StudySeason from './StudySeason/StudySeason';
@@ -14,6 +15,7 @@ const Home = () => {
             <Banner></Banner>
             <EffectiveVisaSolution></EffectiveVisaSolution>
             <FeaturedServices></FeaturedServices>
+            <CreditTransfer></CreditTransfer>
             <OurTeam></OurTeam>
         </div>
     );
