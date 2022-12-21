dc6c058fce6bedc2472d1aed68545fa29445cc39
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-12-21 17:00:00 +0600

    started credit transfer section

--- src/Pages/Home/Home/CreditTransfer/CreditTransfer.js
+++ src/Pages/Home/Home/CreditTransfer/CreditTransfer.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import aus_flag from '../../../../assets/creditTransfer/AUS_flag.png';
+import australia from '../../../../assets/creditTransfer/australia.jpg';
+import news_flag from '../../../../assets/creditTransfer/news_flag.png';
+import newzeland from '../../../../assets/creditTransfer/newzealand.jpg';
+import swit_flag from '../../../../assets/creditTransfer/swit_flag.png';
+import switzeland from '../../../../assets/creditTransfer/switzerland.jpg';
+import uk from '../../../../assets/creditTransfer/uk1.jpg';
+import uk_flag from '../../../../assets/creditTransfer/uk-flag.jpg';
+import usa from '../../../../assets/creditTransfer/usa.jpg';
+import usa_flag from '../../../../assets/creditTransfer/usa_flag.png';
+import world from '../../../../assets/creditTransfer/world.png';
+
 
 const creditTransfer = () => {
     return (
-        <div>
-            <h1 className='text-white text-5xl text-center'>This is Credit transfer section</h1>
+        <div className='h-screen'>
+            <div className='bg-gradient-to-r from-cyan-500 to-blue-500 h-1/2'>
+                <h1>This is writing and World Map section</h1>
+            </div>
+            <div className='bg-gradient-to-r from-red-500 to-green-500 h-1/2'>
+                 <h1>This is card + btn + carousel section</h1>
+            </div>
         </div>
     );
 };
