cd42d720a4162d523c097297c04577e2053549e3
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-12-22 16:09:35 +0600

    finished browse courses writing section

--- src/Pages/Home/Home/BrowseCourses/BrowseCourses.js
+++ src/Pages/Home/Home/BrowseCourses/BrowseCourses.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const BrowseCourses = () => {
     return (
-        <div className='bg-gradient-to-r from-purple-500 to-pink-500 h-screen'>
-            <div className='bg-gradient-to-r from-green-500 to-red-500 h-2/6'>
+        <div className=' h-screen'>
+            <div className=' h-2/6'>
 
-                <h2 className='text-white text-2xl  '>This is Browse courses writing section</h2>
+                <h2 className='text-white text-xl uppercase underline text-center mt-6 underline-offset-2 '>browse courses</h2>
+                 <h2 className='text-white text-4xl mt-4 text-center capitalize'>browse all the courses</h2>
                    </div>
                 <div className='text-white text-2xl h-4/6 bg-gradient-to-r from-red-500 to-green-500 '>
                     <h2>This is Browse card section</h2>
