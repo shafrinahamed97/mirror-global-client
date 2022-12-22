a992d8d7195acbdff0696b429f868d4290154969
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-12-22 17:43:46 +0600

    finished browser courses section

--- src/Pages/Home/Home/BrowseCourses/BrowseCourses.js
+++ src/Pages/Home/Home/BrowseCourses/BrowseCourses.js
@@ -3,16 +3,160 @@ import React from 'react';
 const BrowseCourses = () => {
     return (
         <div className=' h-screen'>
-            <div className=' h-2/6'>
+            <div className=' h-1/6'>
 
                 <h2 className='text-white text-xl uppercase underline text-center mt-6 underline-offset-2 '>browse courses</h2>
                  <h2 className='text-white text-4xl mt-4 text-center capitalize'>browse all the courses</h2>
                    </div>
-                <div className='text-white text-2xl h-4/6 bg-gradient-to-r from-red-500 to-green-500 '>
-                    <h2>This is Browse card section</h2>
-                </div>
-        </div>
-    );
-};
+                <div className='text-white text-2xl h-5/6 ml-6 mr-6'>
+                    
+                <div class="grid lg:grid-cols-3 gap-4 md:grid-col-2   h-screen">
+                          <div className='mt-6'>
+                          <div className="card  glass">
+  
+  <div className="card-body">
+    <h2 className="card-title capitalize">diploma programs</h2>
+    <p className='capitalize text-base'>diploma programs with other family based and investment
+     finally has got immigration with job</p>
+    <div className="card-actions justify-start">
+      <button className="btn btn-primary">view programs</button>
+    </div>
+   
+    
+     <div>
+     
+   
+  </div>
+  </div>
+                    </div>
+                          </div>
+                          <div className='mt-6'>       
+                            <div className="card  glass">
+  
+                <div className="card-body">
+              <h2 className="card-title capitalize">graduate programs</h2>
+            <p className='capitalize text-base'>diploma programs with other family based and investment
+              finally has got immigration with job</p>
+             <div className="card-actions justify-start">
+           <button className="btn btn-primary">view programs</button>
+           </div>
+           <div>
+           </div>
+           </div>
+            </div>
+                   </div>
+                   
+                   <div className='mt-6'>      
+                             <div className="card  glass">
+  
+          <div className="card-body">
+       <h2 className="card-title capitalize">undergraduate programs</h2>
+        <p className='capitalize text-base'>diploma programs with other family based and investment
+        finally has got immigration with job</p>
+        <div className="card-actions justify-start">
+      <button className="btn btn-primary">view programs</button>
+       </div>
 
-export default BrowseCourses;
+       <div>
+       </div>
+  </div>
+    </div>
+     </div>
+
+     <div>      
+    <div className=''>      
+       <div className="card  glass">
+       <div className="card-body">
+       <h2 className="card-title capitalize">postgraduate programs</h2>
+        <p className='capitalize text-base'>diploma programs with other family based and investment
+        finally has got immigration with job</p>
+        <div className="card-actions justify-start">
+      <button className="btn btn-primary">view programs</button>
+       </div>
+       <div>
+       </div>
+           </div>
+         </div>
+         </div>
+      </div>
+      <div>     
+         <div className=''>      
+                            
+     <div className="card  glass">
+     <div className="card-body">
+       <h2 className="card-title capitalize">research programs</h2>
+        <p className='capitalize text-base'>diploma programs with other family based and investment
+        finally has got immigration with job</p>
+        <div className="card-actions justify-start">
+      <button className="btn btn-primary">view programs</button>
+       </div>
+       <div>
+         
+  </div>
+  </div>
+</div></div></div>
+                          <div>      <div className=''>      
+                             <div className="card  glass">
+  
+          <div className="card-body">
+       <h2 className="card-title capitalize">PHD programs</h2>
+        <p className='capitalize text-base'>diploma programs with other family based and investment
+        finally has got immigration with job</p>
+        <div className="card-actions justify-start">
+      <button className="btn btn-primary">view programs</button>
+       </div>
+       <div>
+       </div>
+     </div>
+
+     </div>
+   </div>
+   </div>
+   
+   
+   </div>
+                   </div>
+           </div>
+       );
+   };
+   
+   export default BrowseCourses;
+   
+     
+     
+   
+     
+   
+                             
+                             
+               
+   
+         
+             
+     
+   
+               
+   
+    
+    
+   
+  
+                  
+  
+          
+   
+    
+    
+     
+   
+ 
+                          
+  
+        
+   
+    
+     
+     
+  
+    
+   
