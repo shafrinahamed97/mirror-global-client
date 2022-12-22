ec9c1c19769caec0ba0834edf5a4c732b824bd48
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-12-22 16:02:30 +0600

    stared browser courses section

---
+++ src/Pages/Home/Home/BrowseCourses/BrowseCourses.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const BrowseCourses = () => {
+    return (
+        <div className='bg-gradient-to-r from-purple-500 to-pink-500 h-screen'>
+            <div className='bg-gradient-to-r from-green-500 to-red-500 h-2/6'>
+
+                <h2 className='text-white text-2xl  '>This is Browse courses writing section</h2>
+                   </div>
+                <div className='text-white text-2xl h-4/6 bg-gradient-to-r from-red-500 to-green-500 '>
+                    <h2>This is Browse card section</h2>
+                </div>
+        </div>
+    );
+};
+
+export default BrowseCourses;
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import EffectiveVisaSolution from '../EffectiveVisaSolution/EffectiveVisaSolution';
+import BrowseCourses from './BrowseCourses/BrowseCourses';
 
 import CreditTransfer from './CreditTransfer/CreditTransfer';
 import FeaturedServices from './FeaturedServices/FeaturedServices';
@@ -15,6 +16,7 @@ const Home = () => {
             <Banner></Banner>
             <EffectiveVisaSolution></EffectiveVisaSolution>
             <FeaturedServices></FeaturedServices>
+            <BrowseCourses></BrowseCourses>
             <CreditTransfer></CreditTransfer>
             <OurTeam></OurTeam>
         </div>
