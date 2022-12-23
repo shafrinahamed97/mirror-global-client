341a7f9b06de622870ed0133dddf194cceaf412e
shafrinahamed97 <shafrinahamedjob@gmail.com>
2022-12-23 16:26:54 +0600

    finished online visa immigration section

--- src/Pages/Home/Home/OnlineVisaImmigration/OnlineVisaImmigration.js
+++ src/Pages/Home/Home/OnlineVisaImmigration/OnlineVisaImmigration.js
@@ -2,8 +2,50 @@ import React from 'react';
 
 const OnlineVisaImmigration = () => {
     return (
-        <div className='h-screen  bg-gradient-to-r from-purple-500 to-pink-500'>
-            <h1 className='text-white text-2xl text-center'>This is Online VISA Immigration Section</h1>
+        <div className='h-30  '>
+           <div class="grid lg:grid-cols-4 gap-4 pt-12 ml-4 mr-4 md:grid-cols-1 ">
+           <div>
+            <div className="card w-64 bg-primary text-primary-content">
+            <div className="card-body">
+            <h2 className="card-title text-2xl">online visa application</h2>
+              
+          
+      </div>
+        </div>
+         </div>
+           <div>
+            <div>
+            <div className="card  w-64 bg-primary text-primary-content  ">
+            <div className="card-body">
+            <h2 className="card-title  text-2xl">visa information</h2>
+              
+          
+      </div>
+        </div>
+         </div>
+         </div>
+          <div><div>
+            <div className="card w-64 bg-primary text-primary-content ">
+            <div className="card-body">
+            <h2 className="card-title">immigration resources</h2>
+              
+          
+      </div>
+        </div>
+         </div>
+         </div>
+         <div><div>
+            <div className="card w-64 bg-primary text-primary-content">
+            <div className="card-body">
+            <h2 className="card-title">online passport application</h2>
+          
+      </div>
+        </div>
+         </div></div>
+  
+  
+  
+</div>
         </div>
     );
 };
